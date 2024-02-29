050cb5ca31dde4b3c60e0d884a20736efaa05d09
Cristian Vergara <85785449+cverbah@users.noreply.github.com>
2024-02-29 19:42:27 -0300

    add imgs extractor

--- scrapper.py
+++ scrapper.py
@@ -8,6 +8,50 @@ import pandas as pd
 import time
 import urllib
 import requests
+import re
+
+## functions ###
+
+def scroll_all_website(driver, scroll_increment=100, scroll_delay=0.25): # scroll from top to bottom
+    page_height = driver.execute_script("return document.body.scrollHeight")
+    print(f'page length: {page_height}')
+    current_position = 0
+
+    while current_position < page_height:
+        driver.execute_script(f"window.scrollTo(0, {current_position});")
+        time.sleep(scroll_delay)
+
+        current_position += scroll_increment
+        scanned = round(current_position/page_height, 2)
+        print(f'scanned: {scanned}')
+        if scanned > 0.1:  # speed up
+            scroll_increment = 300
+            scroll_delay = 1
+
+
+def get_imgs(soup):
+    elements = soup.find_all('div', attrs={"class": "lazyload-wrapper"})
+    all_imgs = []
+    for element in elements:
+        if element.picture:
+            element_dict = {}
+            element_dict['name_img'] = element.picture.find('img').get('alt')
+            element_dict['url_img'] = element.picture.find('source').get('srcset')
+            all_imgs.append(element_dict)
+
+    df = pd.DataFrame(all_imgs)
+    return df
+
+
+blacklist = ['falabella', 'sodimac', 'tottus', 'cmr', 'nosotros', 'ecosistema', 'seguros', 'puntospesos']
+def clean_name(row, blacklist=blacklist):
+    row = str(row)
+    tokens = re.findall(r"(?=("+'|'.join(blacklist)+r"))", row)
+    if len(tokens) > 0:
+      return 'blacklisted'
+    else:
+      return row
+
 
 def main():
     ''''testing'''
@@ -21,30 +65,21 @@ def main():
     try:
         driver.get(url)
         time.sleep(1)
-        page_height = driver.execute_script("return document.body.scrollHeight")
 
-        # for loading all images (lazy load wrapper)
-        # define the scroll increment and delay between scrolls
-        scroll_increment = 100
-        scroll_delay = 0.25
+        scroll_all_website(driver, scroll_increment=20, scroll_delay=1)
 
-        # wcroll from top to bottom
-        current_position = 0
-        while current_position < page_height:
-
-            driver.execute_script(f"window.scrollTo(0, {current_position});") # Scroll down by the scroll_increment
-            time.sleep(scroll_delay)
-
-            current_position += scroll_increment # update the current position
-
-        # get code with loaded items
+        # get code with lazyload-wrappers imgs loaded
         website_code = driver.page_source
         with open('webpage_source.txt', 'w', encoding='utf-8') as file:
             file.write(website_code)
         driver.quit()
 
         soup = BeautifulSoup(website_code, 'html.parser')
-        print(soup.prettify())
+        #print(soup.prettify()) #for developing
+        df_imgs = get_imgs(soup)
+        df_imgs['name_img'] = df_imgs['name_img'].apply(lambda row: clean_name(row))
+        df_imgs = df_imgs[df_imgs.name_img != 'blacklisted'].reset_index(drop=True)  # filter out
+        print(df_imgs.head(10))
 
     except Exception as e:
         print(e)
