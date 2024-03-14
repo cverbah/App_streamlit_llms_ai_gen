d539e6d75cb9a6c50dd33bf1ed71a519b9cb56c3
Cristian Vergara <85785449+cverbah@users.noreply.github.com>
2024-03-14 19:32:44 -0300

    add plots

--- utils_plots.py
+++ utils_plots.py
@@ -5,8 +5,33 @@ from wordcloud import WordCloud
 from unidecode import unidecode
 import matplotlib
 matplotlib.use('Agg')
-import streamlit as st
 import plotly.express as px
+
+
+def plot_col_against_date(df, col, plot_type='bar', top=5, height=400, width=600):
+    assert plot_type in ['bar', 'line'], 'wrong type of chart, suported: bar, line'
+    dates = df['date_checked'].unique().tolist()
+    data = []
+    for date in dates:
+        col_list = df[col][df.date_checked == date].tolist()
+        col_list_flatten = [val for sublist in col_list for val in sublist]
+        col_list_flatten_format = list(map(lambda x: unidecode(x.lower()), col_list_flatten))
+        count = Counter(col_list_flatten_format).most_common()[:top]
+        df_date = pd.DataFrame(count, columns=[f'{col}', 'total'])
+        df_date['date_checked'] = date
+        data.append(df_date)
+
+    df_reshaped = pd.concat(data)
+    df_reshaped = df_reshaped.sort_values(by=['date_checked', 'total', f'{col}'], ascending=[True, False, True])
+    aux = ' '.join(col.split('_'))
+    if plot_type == 'bar':
+        fig = px.bar(df_reshaped, x='date_checked', y='total', color=f'{col}', height=height, width=width, title=f'Top {top}: {aux}/día')
+
+    if plot_type == 'line':
+        fig = px.line(df_reshaped, x="date_checked", y="total", color=f'{col}', symbol=f'{col}', markers=True,
+                      height=height, width=width, title=f'Top {top}: {aux}/día')
+
+    return fig
 
 
 def plot_wordcloud(df, col, color='black', max_words=20, height=200):
@@ -22,7 +47,7 @@ def plot_wordcloud(df, col, color='black', max_words=20, height=200):
     return plt
 
 
-def plot_against_offer_type(df, col, top=10):
+def plot_against_offer_type(df, col, top=10, height=200, width=200):
     assert col in ['categorias_en_promo', 'marcas_en_promo', 'publico_objetivo'], 'wrong key'
     keywords = df[col].tolist()
     keywords_flatten = [val for sublist in keywords for val in sublist]
@@ -31,7 +56,8 @@ def plot_against_offer_type(df, col, top=10):
     df_counter = pd.DataFrame(col_counter, columns=[f'{col}', 'total'])
     df_counter = df_counter.sort_values(by='total', ascending=True)
 
-    fig = px.bar(df_counter, x="total", y=f"{col}")
+    aux = ' '.join(col.split('_'))
+    fig = px.bar(df_counter, x="total", y=f"{col}", height=height, width=width, title=f'Top {top}')
     return fig
 
         
